53750f40f07a42cd50213eda4b4ff6405506eb38
sknank <saif.khan95@hotmail.com>
2019-11-26 10:44:31 +0100

    Added controllers for userhome, and created a view for userhome

--- PlantRMVC2/Controllers/HomeController.cs
+++ PlantRMVC2/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using PlantRMVC2.PlantRRef;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
 
 namespace PlantRMVC2.Controllers
 {
@@ -24,6 +26,59 @@ namespace PlantRMVC2.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult UserHome()
+        {
+            ViewBag.Message = "Your User Home page.";
+            ViewData["Plants"] = new List<PersonalPlant>
+            {
+                new PersonalPlant
+                {
+                ID=1,
+                AId=99,
+                CName = "Rose",
+                Description = "Apparently everyone has its thorn, or something",
+                ImageURL = "http://www.flowerpicturesfromMatesmum.com",
+                LName = "Rosa",
+                SDays = 7,
+                LastWatered = DateTime.Now,
+                NextWatered= DateTime.Now.AddDays(3),
+                NName="Roseyrose",
+                WDuration=3,
+                },
+                new PersonalPlant
+                {
+                ID = 2,
+                AId = 99,
+                CName = "Tulip",
+                Description = "Tulipingn",
+                ImageURL = "http://www.flowerpicturesfromMatesmum.com",
+                LName = "Tulipa",
+                SDays = 7,
+                LastWatered = DateTime.Now,
+                NextWatered = DateTime.Now.AddDays(3),
+                NName = "Tilly",
+                WDuration = 3,
+                },
+                new PersonalPlant
+                {
+                    ID = 3,
+                AId = 99,
+                CName = "Magnolia",
+                Description = "Magdelene died for this",
+                ImageURL = "http://www.flowerpicturesfromMatesmum.com",
+                LName = "Magni",
+                SDays = 7,
+                LastWatered = DateTime.Now,
+                NextWatered = DateTime.Now.AddDays(3),
+                NName = "Maggy",
+                WDuration = 3,
+                }
+
+            };
+            
             return View();
         }
     }
---
+++ PlantRMVC2/Scripts/tables.js
@@ -0,0 +1,20 @@
+﻿var gridData = {
+    Head: [["Header 1", "Header 2", "Header 3"]],
+    Body: [["Row 1, Cell 1", "Row 1, Cell 2", "Row 1, Cell 3"],
+        ["Row 2, Cell 1", "Row 2, Cell 2", "Row 2, Cell 3"],
+    ["Row 3, Cell 1", "Row 3, Cell 2", "Row 3, Cell <em>3</em>"]]
+};
+
+
+
+new Grid("myGrid", {
+    srcType: "json",
+    srcData: gridData,
+    allowGridResize: true,
+    allowColumnResize: true,
+    allowClientSideSorting: true,
+    allowSelections: true,
+    allowMultipleSelections: true,
+    showSelectionColumn: true,
+    fixedCols: 1
+});
