9d90c84a834fb4ad7e1b97297748e1789ff11047
sknank <saif.khan95@hotmail.com>
2019-12-04 00:54:18 +0100

    helped stevo with viewdata

--- PlantRMVC2/Controllers/UserController.cs
+++ PlantRMVC2/Controllers/UserController.cs
@@ -26,6 +26,9 @@ namespace PlantRMVC2.Controllers
         {
             // Make a thing to get a list of all the plants, I guess? I don't fucking know. I mean how the fuck are you supposed to pass
             // in more than one model? I guess with a ViewData things like BigDickSaif did. btw he's gay. 
+            //dont be rude - saif
+            ViewData["StockPlants"] = service.GetAllPlants();
+            //hopefully something like this^^^^-saif
             return View();
         }
 
